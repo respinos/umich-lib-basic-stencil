0dcd50393827fc88e402a9be20ad60c18b06d7d6
Roger Espinosa <roger@umich.edu>
2019-11-21 17:16:21 -0500

    humbug

--- dist/umich-lib-basic-stencil/umich-lib-basic-stencil.esm.js
+++ dist/umich-lib-basic-stencil/umich-lib-basic-stencil.esm.js
@@ -1,6 +1,1 @@
-import { p as patchBrowser, g as globals, b as bootstrapLazy } from './core-ae485377.js';
-
-patchBrowser().then(options => {
-  globals();
-  return bootstrapLazy([["my-component",[[1,"my-component",{"first":[1],"middle":[1],"last":[1]}]]],["umichlib-universal-header",[[0,"umichlib-universal-header",{"isOpen":[1028,"is-open"],"isLoaded":[1028,"is-loaded"],"data":[1032],"error":[1028]}]]]], options);
-});
+import{p as a,b as s}from"./p-32ccc5aa.js";a().then(a=>s([["p-fzhxbsxa",[[1,"my-component",{first:[1],middle:[1],last:[1]}],[0,"umichlib-universal-header",{isOpen:[1028,"is-open"],isLoaded:[1028,"is-loaded"],data:[1032],error:[1028]}]]]],a));
